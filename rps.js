ac8b3c908024d9f9ac7c44c0f920b95b9768a5a2
mentipento <26530260+mentipento@users.noreply.github.com>
2024-11-07 16:25:59 +0100

    create working function getComputerChoice()

--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+
+function restart() {
+
+    if ((Math.floor(Math.random()*10) === 1 || 2 || 3) === true) {
+        return "Rock";
+        } else if ((Math.floor(Math.random()*10) === 4 || 5 || 6) === true) {
+            return "Paper";
+        } else if ((Math.floor(Math.random()*10) === 7 || 8 || 9) === true) {
+            return "Scissors";
+        } else restart();
+    }
+    
+restart()
+
